cca249fa6987688c5aef92674b8c15546d7503ba
ENDAVA\sanja.jovanovic <sanja.jovanovic@endava.com>
2017-10-13 17:18:34 +0200

    Changes

--- LocationTracker/app/src/main/java/com/locationtracker/myapp/locationtracker/MyTracksActivity.java
+++ LocationTracker/app/src/main/java/com/locationtracker/myapp/locationtracker/MyTracksActivity.java
@@ -1,7 +1,9 @@
 package com.locationtracker.myapp.locationtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyTracksActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MyTracksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_tracks);
+    }
+
+
+    public void NewTrack(View view){
+        Intent intent = new Intent(this, NewTrackActivity.class);
+        startActivity(intent);
     }
 }
